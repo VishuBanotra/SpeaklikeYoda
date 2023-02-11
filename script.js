40f4d04f982525da7ff6e02e79d35a547ab81abc
Vishu Banotra <vishuvishalbanotra@gmail.com>
2023-02-11 22:02:45 +0530

    added script and translate page

---
+++ script.js
@@ -0,0 +1,25 @@
+let inputText = document.querySelector("#text-area");
+let button = document.querySelector("#btn-translate");
+let output_text = document.querySelector(".output-area");
+
+let serverURL = "https://api.funtranslations.com/translate/yoda.json";
+
+function translatedTextUrl(input) {
+  return serverURL + "?" + "text=" + input;
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("There is an issue with the server");
+}
+
+button.addEventListener("click", function () {
+  let txt_input = inputText.value;
+  fetch(translatedTextUrl(txt_input))
+    .then((response) => response.json())
+    .then((json) => {
+        let trasnlatedText = json.contents.translated;
+        output_text.innerHTML = trasnlatedText;
+    })
+    .catch(errorHandler)
+});
